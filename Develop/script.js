9ac94668d743c75cc36223b479850d3b599695cf
Alicen Bender <alicene.bender@gmail.com>
2023-12-03 10:57:45 -0500

    Have added a prompt to the generatePassword function and have set up the spot for return.

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,17 @@
 // Assignment code here
+// Password and length is > 8 and < 128 characters.
+// Characters can include lowercase, uppercase, numeric and/special characters.
+// Password is generated once all prompts are complete.
+// Password is displayed on the screen.
+function generatePassword() {
+  console.log("Button clicked");
+//When the generate password button is clicked, a prompt shows up.
+  var passwordLength = prompt("How many characters would you like your password to be? (Between 8-128 characters)");
 
+
+//Need to return the generated password (temporary placeholder)
+  return "Generated password";
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
