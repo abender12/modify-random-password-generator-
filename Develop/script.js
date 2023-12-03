42b070aa7fe2635b120e4e3019383cdb550cd058
Alicen Bender <alicene.bender@gmail.com>
2023-12-03 11:07:29 -0500

    Added necessary prompts for password criteria.

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,17 @@
 // Assignment code here
-// Password and length is > 8 and < 128 characters.
-// Characters can include lowercase, uppercase, numeric and/special characters.
-// Password is generated once all prompts are complete.
-// Password is displayed on the screen.
 function generatePassword() {
   console.log("Button clicked");
-//When the generate password button is clicked, a prompt shows up.
-  var passwordLength = prompt("How many characters would you like your password to be? (Between 8-128 characters)");
+//When the generate password button is clicked, prompts show up.
+  // Password and length is > 8 and < 128 characters.
+  // Characters can include lowercase, uppercase, numeric and/special characters.
+  var passwordLength = prompt("How many characters would you like your password to be? (Minimum 8 characters)");
+  var lowercase = prompt("Do you want lowercase letters in your password?");
+  var uppercase = prompt("Do you want uppercase letters in your password?");
+  var numeric = prompt("Do you want numbers in your password?");
+  var specialChar = prompt("Do you want special characters in your password?");
+// Make sure that the password is in the correct range of characters and that at least one of the character type is selected.
 
+// Generate password based on criteria.
 
 //Need to return the generated password (temporary placeholder)
   return "Generated password";
